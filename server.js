890f0f396aa8eef5d743adaf1f313d4c766e81d9
sharmaprinceji <princesh1411@gmail.com>
2026-02-18 18:02:19 +0530

    add cors

--- server.js
+++ server.js
@@ -13,7 +13,14 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://micro-frontend-self.vercel.app"
+  ],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // REGISTER ROUTES
